c6cd610169fac11445adcf78049b09d502bec1f3
Cameron Worthington <cameron.worthington@bbd.co.za>
2024-05-01 15:45:48 +0200

    Fix: allowed using <a> to navigate out to absolute URLs and fixed routing to allow navigating to any page in the app as the first load

--- public/src/_routing/start.js
+++ public/src/_routing/start.js
@@ -1,14 +1,28 @@
-import { render } from "./render.js";
+import { routes } from "../routes.js";
 const BaseURL = window.location.href;
-export function start() {
-    const currentURL = window.location.href;
-    if(currentURL == BaseURL) {
-        render('');
-        return;
+const parts = BaseURL.split('/');
+const firstRoute=parts[3] ?? '' ;
+const domain = `${parts[0]}//${parts[2]}/`;
+/**
+ * 
+ * @param {*} route either a route defined in the route.js, or a link starting with http for redirect.
+ */
+export function navigate(route) {
+    if (routes[route]) {
+        const app = document.getElementById('app');
+        app.innerHTML = `<${routes[route].identifier}></${routes[route].identifier}>`;
+    } else if (route.includes('http')){
+        window.location.href = route;
+    } 
+    else {
+        console.error(`Page ${route} not found.`);
+        if (route == ''){
+            throw new Error(`UNRECOVERABLE ROUTING STATE: The site is failing to route to the ('') homepage. This should only happen if the '' route is not in the routes lookup in /routes.js `)
+        }else{
+            navigate(''); 
+        }
     }
-    const parts = currentURL.split('/');
-    const route = parts[parts.length - 1];
-    render(route);
+    history.pushState(null, null, `${domain}${route}`);
 }
 
-render('');
+navigate(firstRoute);
