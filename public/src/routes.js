1015797f4a48f3e02af60c19f5b8c54233b3b011
shane-theron-bbd <shane.theron@bbd.co.za>
2024-05-05 19:39:32 +0200

    Created Spider List page

--- public/src/routes.js
+++ public/src/routes.js
@@ -2,10 +2,12 @@ import { BasePage } from "./pages/Base/BasePage.js";
 import { HomePage } from "./pages/Home/HomePage.js";
 import { LoginPage } from "./pages/Login/LoginPage.js";
 import { ContactPage } from "./pages/Contact/ContactPage.js";
+import { SpiderList } from "./pages/SpiderList/SpiderListPage.js";
 
 export const routes = {
     '': BasePage,
     'home': HomePage,
     'login': LoginPage,
-    'contact': ContactPage
+    'contact': ContactPage,
+    'spiderlist' : SpiderList,
 }
