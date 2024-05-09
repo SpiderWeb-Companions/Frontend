ef6116951fc84938338c881b52a6de16dea59be1
Cameron Worthington <cameron.worthington@bbd.co.za>
2024-05-09 10:45:03 +0200

    fix: change to secretes

--- public/src/pages/Login/LoginPage.js
+++ public/src/pages/Login/LoginPage.js
@@ -4,42 +4,34 @@ import { setAccessToken, isAuthenticated, getUserDetails} from "../../_authentic
 import { enableRouting } from "../../_routing/start.js";
 
 
-    let identifier = 'login-page';
-    let authUrl =  process.env.AUTH_URL;
-    let scope = process.env.SCOPE;
-    let clientId = process.env.CLIENT_ID;
-    let redirectUri = process.env.REDIRECT_URI;
-
-
-
-    export async function LoginPage(queryString) {
-        let authenticated = await isAuthenticated(); //add this to your component if you want it to check for authentication.
-        const app = document.getElementById('app');
-        app.innerHTML = `<h1> Loading</h1>`
-        if (queryString && !authenticated ) {
-            const params = new URLSearchParams(queryString);
-            let code = params.get('code')
-            
-            setAccessToken(code);
-           
-            app.innerHTML =  html`
-                <h2>Waiting for authorisation</h2>
-            `
-        } else if (authenticated){
-            let userInfo = await getUserDetails();
-            app.innerHTML =  html`
-            <h1>You've been successfully authenticated!!</h1>
-            <p> ${userInfo.name} </p>
-            `
-        }else{
-
+export async function LoginPage(queryString) {
+    let authenticated = await isAuthenticated(); //add this to your component if you want it to check for authentication.
+    const app = document.getElementById('app');
+    app.innerHTML = `<h1> Loading</h1>`
+    if (queryString && !authenticated ) {
+        const params = new URLSearchParams(queryString);
+        let code = params.get('code')
+        
+        setAccessToken(code);
+        
         app.innerHTML =  html`
-            <h1>SpiderWeb Companions</h1>
-            <a href="${authUrl}?scope=${scope}&response_type=code&redirect_uri=${redirectUri}&client_id=${clientId}">Google authentication</a>
+            <h2>Waiting for authorisation</h2>
         `
-        }
-        enableRouting('a')
+    } else if (authenticated){
+        let userInfo = await getUserDetails();
+        app.innerHTML =  html`
+        <h1>You've been successfully authenticated!!</h1>
+        <p> ${userInfo.name} </p>
+        `
+    }else{
+
+    app.innerHTML =  html`
+        <h1>SpiderWeb Companions</h1>
+        <a href="${AUTH_URL}?scope=${SCOPE}&response_type=code&redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}">Google authentication</a>
+    `
     }
+    enableRouting('a')
+}
 
 
     
